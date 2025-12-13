62d0519655853a82641e27b99cf72336b8af194c
Aezrev <111585670+Aezrev@users.noreply.github.com>
2025-12-13 18:04:33 +0200

    Updated Comments
    -Comentarii pentru NoteTag si GroupMember

--- Models/GroupMember.js
+++ Models/GroupMember.js
@@ -1,3 +1,19 @@
+/*
+Tabela intermediara GroupMember
+-Leaga User de Group
+Campuri
+-id(implicit,PK)
+-role
+-UserId(FK catre User)
+-GroupId(FK catre Group)
+-createdAt(implicit)
+-updatedAt(implicit)
+Relatii
+-User poate sa fie membru in mai multe Groups
+-Group poate sa aiba mai multi Users
+*/
+
+
 export const GroupMemberModel = (sequelize) => {
     return sequelize.define("GroupMember", {
         role: { type: DataTypes.STRING, defaultValue: "member" }, // admin/member
--- Models/NoteTag.js
+++ Models/NoteTag.js
@@ -1,3 +1,18 @@
+/*
+Tabela intermediara NoteTag
+-leaga Note de Tag
+Campuri:
+-id(implicit, PK)
+-NoteId(FK catre note)
+-TagId(FK catre Tag)
+-createdAt(implicit)
+-updatedAt(implicit)
+Relatii:
+-Note poate avea mai multe Tag
+-Tag poate fi asociat cu mai multe Note
+*/
+
+
 export const NoteTagModel = (sequelize) => {
     return sequelize.define("NoteTag", {});
 };
